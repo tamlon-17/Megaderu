1e18d7739dedb4d0327d6bb9c09a229ff9529c4f
tamlon-17 <tamlon17@gmail.com>
2025-02-24 17:21:09 +0900

    コードをスリム化2

--- megaderu.py
+++ megaderu.py
@@ -11,7 +11,8 @@ st.set_page_config(page_title='めがで～る 2025', page_icon='icon.ico',
                    initial_sidebar_state='expanded')
 st.title('めがで～る 2025')
 st.caption('これは、乾田直播の出芽を予測するウェブアプリ（2025年版）です。')
-st.caption('予測がはずれても責任は一切取りませんので、ご了承のうえお使いください。')
+st.caption(
+    '予測がはずれても責任は一切取りませんので、ご了承のうえお使いください。')
 st.caption('作成者：しがない普及指導員')
 st.text('👈👈左側を入力すると、予測結果が変わるよ！！')
 st.subheader('予測結果')
@@ -19,13 +20,16 @@ st.subheader('予測結果')
 # 使用する年を今年に設定
 this_year = date.today().year
 # アメダス地点をリスト化と辞書化（気象庁の並びで統一）
-amedas_l = ['気仙沼', '川渡', '築館', '志津川', '古川', '大衡', '鹿島台', '石巻', '新川', '仙台', '白石', '亘理', '米山', '塩釜', '駒ノ湯', '丸森', '名取', '蔵王', '女川']
+amedas_l = ['気仙沼', '川渡', '築館', '志津川', '古川', '大衡', '鹿島台',
+            '石巻', '新川', '仙台', '白石', '亘理', '米山', '塩釜', '駒ノ湯',
+            '丸森', '名取', '蔵王', '女川']
 amedas_dic = dict(気仙沼=1, 川渡=2, 築館=3, 志津川=4, 古川=5, 大衡=6, 鹿島台=7,
                   石巻=8, 新川=9, 仙台=10, 白石=11, 亘理=12, 米山=14, 塩釜=15,
                   駒ノ湯=16, 丸森=17, 名取=20, 蔵王=23, 女川=24)
 
 # 西部の市町村をリスト化
-east_city = ['泉区', '白石市', '蔵王町', '七ヶ宿町', '川崎町', '大和町', '大衡村', '色麻町', '加美町']
+east_city = ['泉区', '白石市', '蔵王町', '七ヶ宿町', '川崎町', '大和町',
+             '大衡村', '色麻町', '加美町']
 # 市町村と気象協会のコードの辞書
 city_dic = dict(仙台市=4100, 青葉区=4101, 宮城野区=4102, 若林区=4103,
                 太白区=4104, 泉区=4105, 白石市=4206, 角田市=4208, 蔵王町=4301,
@@ -77,6 +81,7 @@ def scrape_rain(month, s_day, e_day):
     return rl
 
 
+# スクレイプする関数を同時に動かす関数
 def screpe(month, s_day, e_day):
     tl = scrape_temp(month, s_day, e_day)
     rl = scrape_rain(month, s_day, e_day)
@@ -87,29 +92,29 @@ def screpe(month, s_day, e_day):
 # 播種日が使用日の前日より前で播種月と使用月が同じ場合
 yesterday_d = today_d - timedelta(days=1)
 if seeding_d < yesterday_d and seeding_d.month == yesterday_d.month:
-    past_temp, past_rain = screpe(seeding_d.month, seeding_d.day, yesterday_d.day)
+    past_t, past_r = screpe(seeding_d.month, seeding_d.day, yesterday_d.day)
 # 播種月が使用月より前の場合
 elif seeding_d.month < today_d.month:
-    past_temp, past_rain = screpe(seeding_d.month, seeding_d.day, 31)
+    past_t, past_r = screpe(seeding_d.month, seeding_d.day, 31)
     sm = seeding_d.month + 1
     # 播種月が使用月の前月の場合
     if sm == today_d.month:
-        past_temp1, past_rain1 = screpe(sm, 0, today_d.day - 1)
-        past_temp += past_temp1
-        past_rain += past_rain1
+        past_t1, past_r1 = screpe(sm, 0, today_d.day - 1)
+        past_t += past_t1
+        past_r += past_r1
     # 播種月が使用月の前々月の場合（3月播種-5月使用か4月播種-6月使用）
     else:
-        past_temp2, past_rain2 = screpe(sm, 0, 31)
-        past_temp += past_temp2
-        past_rain += past_rain2
+        past_t2, past_r2 = screpe(sm, 0, 31)
+        past_t += past_t2
+        past_r += past_r2
         sm += 1
-        past_temp3, past_rain3 = screpe(sm, 0, today_d.day - 1)
-        past_temp += past_temp3
-        past_rain += past_rain3
+        past_t3, past_r3 = screpe(sm, 0, today_d.day - 1)
+        past_t += past_t3
+        past_r += past_r3
 # 播種日が使用日より先の場合
 else:
-    past_temp = []
-    past_rain = []
+    past_t = []
+    past_r = []
 
 # てんきのサイトから利用日から2週間の予想気温を取得
 ew_num = 3420 if city in east_city else 3410
@@ -133,38 +138,35 @@ def get_rain(clas_name):
     return rl
 
 
-max_tmp_list = get_tmp('high-temp')
-min_tmp_list = get_tmp('low-temp')
-forecast_rain = get_rain('precip')
+max_tmp_l = get_tmp('high-temp')
+min_tmp_l = get_tmp('low-temp')
+forecast_r = get_rain('precip')
 # 最高気温と最低気温から平均気温を算出すると、0.3度高くなるので、－0.3している
-forecast_temp = [(x1 + y1) / 2 - 0.3 for (x1, y1) in
-                 zip(max_tmp_list, min_tmp_list)]
+forecast_t = [(x1 + y1) / 2 - 0.3 for (x1, y1) in zip(max_tmp_l, min_tmp_l)]
 
 # CSVから利用日の２週間後からのアメダス平年値を取得
 # CSVをデータフレームに代入
-df_ave_temp = pd.read_csv('temp.csv', encoding='shift_jis')
+df_ave_t = pd.read_csv('temp.csv', encoding='shift_jis')
 
 # 気温のリストの取得開始日（今日の１４日後）を計算
 # 2か年平均と５か年平均の2種類のリストを作成する。
 start_day = (today_d - date(this_year, 3, 1) + timedelta(days=14))
-ave_temp2 = list(df_ave_temp.iloc[start_day.days:, amedas_point_i + 1])
-ave_temp5 = list(df_ave_temp.iloc[start_day.days:, amedas_point_i + 20])
+ave_t2 = list(df_ave_t.iloc[start_day.days:, amedas_point_i + 1])
+ave_t5 = list(df_ave_t.iloc[start_day.days:, amedas_point_i + 20])
 
 # 平均気温、降水量の各リストを結合する
-combine_temp2 = past_temp + forecast_temp + ave_temp2
-combine_temp5 = past_temp + forecast_temp + ave_temp5
-combine_rain = past_rain + forecast_rain
+combine_t2 = past_t + forecast_t + ave_t2
+combine_t5 = past_t + forecast_t + ave_t5
+combine_r = past_r + forecast_r
 # 播種日より前に使用する場合に、播種日から積算を開始する
 if (dd := seeding_d - yesterday_d) >= timedelta():
-    combine_temp2 = combine_temp2[dd.days:]
-    combine_temp5 = combine_temp5[dd.days:]
-    combine_rain = combine_rain[dd.days:]
+    combine_t2 = combine_t2[dd.days:]
+    combine_t5 = combine_t5[dd.days:]
+    combine_r = combine_r[dd.days:]
 
 # 平均気温リストから11.5℃を引いた有効積算リストを作成
-valid_temp2 = [0 if xx * 10 <= 115 else round(xx - 11.5, 1) for xx in
-               combine_temp2]
-valid_temp5 = [0 if xx * 10 <= 115 else round(xx - 11.5, 1) for xx in
-               combine_temp5]
+valid_t2 = [0 if xx * 10 <= 115 else round(xx - 11.5, 1) for xx in combine_t2]
+valid_t5 = [0 if xx * 10 <= 115 else round(xx - 11.5, 1) for xx in combine_t5]
 
 
 # 積算気温が指定の温度になる日とその時の積算気温を返す関数
@@ -183,44 +185,36 @@ def addition_target_temp(vtl, temp):
 
 
 # 積算30℃、50℃、100℃の日付と温度を取得
-temp30_temp2, temp30_date2 = addition_target_temp(valid_temp2, 30)
-temp50_temp2, temp50_date2 = addition_target_temp(valid_temp2, 50)
-temp100_temp2, temp100_date2 = addition_target_temp(valid_temp2, 100)
-temp30_temp5, temp30_date5 = addition_target_temp(valid_temp5, 30)
-temp50_temp5, temp50_date5 = addition_target_temp(valid_temp5, 50)
-temp100_temp5, temp100_date5 = addition_target_temp(valid_temp5, 100)
+temp30_t2, temp30_d2 = addition_target_temp(valid_t2, 30)
+temp50_t2, temp50_d2 = addition_target_temp(valid_t2, 50)
+temp100_t2, temp100_d2 = addition_target_temp(valid_t2, 100)
+temp30_t5, temp30_d5 = addition_target_temp(valid_t5, 30)
+temp50_t5, temp50_d5 = addition_target_temp(valid_t5, 50)
+temp100_t5, temp100_d5 = addition_target_temp(valid_t5, 100)
 # 予測結果の表示
 st.text('直近2か年の平均気温（高温年）で予測すると、')
+st.text(f'30℃に達するのは、{temp30_d2.month}月{temp30_d2.day}日（{temp30_t2}℃）')
+st.text(f'50℃に達するのは、{temp50_d2.month}月{temp50_d2.day}日（{temp50_t2}℃）')
 st.text(
-    f'30℃に達するのは、{temp30_date2.month}月{temp30_date2.day}日（{temp30_temp2}℃）')
-st.text(
-    f'50℃に達するのは、{temp50_date2.month}月{temp50_date2.day}日（{temp50_temp2}℃）')
-st.text(f'100℃に達するのは、{temp100_date2.month}月{temp100_date2.day}日'
-        f'（{temp100_temp2}℃）')
+    f'100℃に達するのは、{temp100_d2.month}月{temp100_d2.day}日（{temp100_t2}℃）')
 st.text('直近5か年の平均気温で予測すると、')
-st.text(
-    f'30℃に達するのは、{temp30_date5.month}月{temp30_date5.day}日（{temp30_temp5}℃）')
-st.text(
-    f'50℃に達するのは、{temp50_date5.month}月{temp50_date5.day}日（{temp50_temp5}℃）')
-st.text(f'100℃に達するのは、{temp100_date5.month}月{temp100_date5.day}日'
-        f'（{temp100_temp5}℃）')
+st.text(f'30℃に達するのは、{temp30_d5.month}月{temp30_d5.day}日（{temp30_t5}℃）')
+st.text(f'50℃に達するのは、{temp50_d5.month}月{temp50_d5.day}日（{temp50_t5}℃）')
+st.text(f'100℃に達するのは、{temp100_d5.month}月{temp100_d5.day}日（{temp100_t5}℃）')
 
 # 積算グラフの表示
 # 2つの気温のリストの100までの積算和をリストで
-delta_date = temp100_date2 - seeding_d
-accumulate_valid_temp2 = [sum(valid_temp2[:n + 1]) for n in
-                          range(delta_date.days + 1)]
-accumulate_valid_temp5 = [sum(valid_temp5[:n + 1]) for n in
-                          range(delta_date.days + 1)]
+delta_d = temp100_d2 - seeding_d
+acu_valid_t2 = [sum(valid_t2[:n + 1]) for n in range(delta_d.days + 1)]
+acu_valid_t5 = [sum(valid_t5[:n + 1]) for n in range(delta_d.days + 1)]
 # CSVのカラム1から播種日のインデックスを取って、グラフのX軸を作る。
-s_index = seeding_d - date(this_year, 3, 1)
-df11 = df_ave_temp.iloc[s_index.days + 1: s_index.days + delta_date.days + 2,
-                        [0]]
+s_i = seeding_d - date(this_year, 3, 1)
+df11 = df_ave_t.iloc[s_i.days + 1: s_i.days + delta_d.days + 2, [0]]
 
 df1 = df11.reset_index(drop=True)
-df2 = pd.Series(accumulate_valid_temp2)
-df3 = pd.Series(accumulate_valid_temp5)
-df4 = pd.Series(combine_rain)
+df2 = pd.Series(acu_valid_t2)
+df3 = pd.Series(acu_valid_t5)
+df4 = pd.Series(combine_r)
 df_chart = pd.concat([df3, df2, df1, df4], axis=1)
 df_chart.columns = ['5か年平均使用', '2か年平均使用', '月日', '降水量']
 
@@ -234,15 +228,15 @@ fig.add_trace(go.Scatter(x=df_chart['月日'], y=df_chart['2か年平均使用']
                          name='2か年(高温年）平均', line=dict(color='orange')))
 
 fig.update_layout(xaxis=dict(title='月/日', dtick=5),
-                  yaxis1=dict(title='有効積算気温（℃）', range=(0, 110), dtick=10),
+                  yaxis1=dict(title='有効積算気温（℃）', range=(0, 110),
+                              dtick=10),
                   yaxis2=dict(title='降水量（mm）', range=(0, 27.5), dtick=5,
                               showgrid=False),
                   legend=dict(x=0.05, y=0.95, traceorder='reversed'))
 
-fig.update_layout(shapes=[dict(type='rect', x0=0, x1=delta_date.days, y0=30,
+fig.update_layout(shapes=[dict(type='rect', x0=0, x1=delta_d.days, y0=30,
                                y1=50, fillcolor='lightgreen', opacity=0.5,
                                layer='below')])
-fig.update_layout(hovermode="x unified")
-
+fig.update_layout(hovermode='x unified')
 
 st.plotly_chart(fig, use_container_width=True)
